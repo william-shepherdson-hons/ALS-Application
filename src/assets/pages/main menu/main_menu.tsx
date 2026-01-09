0d955c39739bbae504324567838cf9cbd172ebec
williamshepherdson <w.shepo@gmail.com>
2026-01-09 15:09:30 +0000

    set up routing

---
+++ src/assets/pages/main menu/main_menu.tsx
@@ -0,0 +1,29 @@
+
+import "../../../App.css";
+
+
+export default function MainMenu() {
+
+
+  return (
+    <main className="container">
+      <h1>Welcome to Adapt math</h1>
+      
+      <div className="mainmenu">
+        <div className="row">
+          <button className="navoptions">
+            Progress
+          </button>
+          <button className="navoptions">
+            Learn
+          </button>
+          <button className="navoptions">
+            Settings
+          </button>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+
